fff3cf916b9f7ffb3c82821a6df4c22e04a3c395
malakar-soham <malakar_soham@outlook.com>
2021-05-11 22:58:42 +0530

    added scheduler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import tasks
 from dotenv import load_dotenv
 import datetime
 from pytz import timezone
@@ -9,7 +10,6 @@ load_dotenv()
 
 client = discord.Client()
 hackNightRegex = "next ((?:s?hr?|cr|u|o)(?:e|a|i|o|u)?c?o?w?(?:k|c|p|t|oo?|u(?:t|p)?)(?:e|a)?(?:s|y|lacka)?(?:\s|-)?(?:n|b)?(?:e|i|oo?|a)?u?(?:k|w|g|c|o|t)?k?a?(?:ey|ht|ky|e|t|wu|o(?:p|t)?|lacka))"
-
 tz = timezone('US/Eastern')
 
 def setTime(date, hours, mins, secs):
@@ -19,29 +19,27 @@ def setTime(date, hours, mins, secs):
 def getSecs(ts):
     return int(datetime.datetime.timestamp(ts))
 
-def isHappening():
+def happeningNow():
 
     time = datetime.datetime.now(tz)
 
     day = time.strftime("%w")
     hours = time.hour
     mins = time.minute
-
-    print(day, hours, mins)
-
     # Check if today is Wednesday
     if day==3:
         # Check if current time is between 15:30 and 23:59 ET
         if (hours > 15 or (hours == 15 and minutes >= 30)) and (hours < 23 or (hours == 23 and minutes <= 59)):
-            return true
+            return True
 
     # Check if today is Saturday
     if day==6:
         # Check if current time is between 20:30 and 23:59 ET
         if (hours >= 20 and mins >= 30) and (hours <= 23 and mins <= 59):
-            return true
+            return True
 
-    return false
+    #return False
+    return True #delete
 
 def nextDate():
     d = datetime.datetime.now(tz)
@@ -55,22 +53,44 @@ def nextDate():
         d = setTime(d, 20, 30, 0); # Sets time to 20:30 Eastern
     return getSecs(d)
 
+def genTimeMessage(textMatch):
+    prompt=''
+    if happeningNow():
+        prompt = f'_{textMatch[1].title()}_ is happening right now, what are you still doing here!? Join the call!\nhttps://hack.af/night'
+    else:
+        nextHackNight = datetime.datetime.fromtimestamp(nextDate())
+        prompt = f"The next _{textMatch[1]}_ is **{nextHackNight.strftime('%b %d at %I:%M %p')}** your time. See you there!"
+    return prompt
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    checkUpdate.start(client.get_channel(int(os.getenv('CHANNEL_TEST'))))
 
 @client.event
 async def on_message(message):
     channel = client.get_channel(int(os.getenv('CHANNEL_TEST')))
-
     if message.author == client.user:
         return
 
     message = message.content
     textMatch = re.search(hackNightRegex,message)
     if textMatch!=None:
-        nextHackNight = datetime.datetime.fromtimestamp(nextDate())
-        prompt = f"The next _{textMatch[1]}_ is **{nextHackNight.strftime('%b %d at %I:%M %p')}** your time. See you there!"
-        await channel.send(prompt)
+       await channel.send(genTimeMessage(textMatch))
+
+@tasks.loop(minutes=5)
+async def checkUpdate(channel):
+    prompt=None
+    db = int(os.getenv('db'))
+    print(db)
+    mutex = int(os.getenv('mutex'))
+    if happeningNow():
+        if db > 0:
+            prompt = f':crescent_moon: It\'s **Hack Night!**\n\nMeet some new people, build something cool, talk about it. There are no prizes or expectations—just have fun!\n\nThe Hack Night call link has been reactivated by yours truly the Night Golem.\n\n:arrow_right: Join Hack Night (https://hack.af/night)'
+            db-= 1
+            await channel.send(prompt)
+    else:
+        db = mutex
+    os.environ['db'] = str(db)
 
 client.run(os.getenv('BOT_TOKEN'))
